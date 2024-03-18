767aef7a5e3c9e0dbc376169a8485560988f942f
kidsmile <l_aphrodite@sina.com>
2024-03-18 19:18:35 +0800

    5.最长回文子串问题，已验证

--- src/dynamic_动态规划/index.test.js
+++ src/dynamic_动态规划/index.test.js
@@ -1,4 +1,5 @@
 const uniquePaths = require('./62route');
+const longestPalindrome = require('./5');
 
 
 test('62 路径问题', () => {
@@ -7,3 +8,6 @@ test('62 路径问题', () => {
   expect(uniquePaths(7, 7)).toBe(924);
 });
 
+test('5 最长回文子串', () => {
+  expect(longestPalindrome('ccc')).toBe('ccc');
+});
