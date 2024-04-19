1741c19988a42479e938a28fd886f9fa90aff9e8
kidsmile <l_aphrodite@sina.com>
2024-04-19 16:05:57 +0800

    1883. 准时抵达会议现场的最小跳过休息次数，已验证

--- src/dynamic_动态规划/dynamic.test.ts
+++ src/dynamic_动态规划/dynamic.test.ts
@@ -1,5 +1,11 @@
 import wordBreak from './139单词拆分';
+import minSkips from './1883准时抵达会议现场的最小跳过休息次数';
 
 test('139 单词拆分', () => {
 	expect(wordBreak('leetcode', ['leet', 'code'])).toBe(true);
+});
+
+test('1883 准时抵达会议现场的最小跳过休息次数', () => {
+	expect(minSkips([1, 3, 2], 4, 2)).toBe(1);
+	expect(minSkips([7, 3, 5, 5], 2, 10)).toBe(2);
 });
